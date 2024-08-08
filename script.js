9c3911e13db77ed096709f2620febb1bd0586e5c
Tannuattri01 <161629643+Tannuattri01@users.noreply.github.com>
2024-08-08 23:57:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+ScrollReveal({
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+
+ScrollReveal().reveal('.hea,ding', { 
+  origin: 'left',
+  opacity: 0,
+  distance: '50px'
+});
+ScrollReveal().reveal('', { 
+  origin: 'left',
+  opacity: 0,
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out' 
+});
+ScrollReveal().reveal('.project__info', { 
+  origin: 'right',
+  opacity: 0,
+  distance: '50px',
+  duration: 1000, 
+  easing: 'ease-in-out'
+});
+
+
+ScrollReveal().reveal(' .headingline-2', { 
+  origin: 'right',
+  opacity: 0,
+  distance: '50px'
+});
+ScrollReveal().reveal('  .btn', { 
+  origin: 'top',
+  opacity: 0,
+  distance: '50px'
+});
+ScrollReveal().reveal('user_pic,.profile__blurb,.skills', { 
+  origin: 'left',
+  opacity: 0,
+  distance: '50px'
+});
+
+ScrollReveal().reveal('.skills', { 
+  origin: 'bottom',
+  opacity: 0,
+  distance: '50px'
+});
